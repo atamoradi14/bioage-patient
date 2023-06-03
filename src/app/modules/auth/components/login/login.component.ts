ce8452401aa2d22e37b6c0cc792e62767a1326ec
Naime Ahmed <naime.ahmed@metadesignsolutions.co.uk>
2023-06-03 21:19:50 +0600

    login form done

--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
 import { AuthService } from '@modules/auth/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
-  constructor(private authService: AuthService, private titleService: Title) {}
+export class LoginComponent implements OnInit {
+  showNewPassword = false;
+  signInFormgroup!: FormGroup;
+
+  constructor(
+    private authService: AuthService,
+    private titleService: Title,
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.signInFormgroup = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+  }
   login() {
     this.authService.login();
+  }
+
+  get f() {
+    return this.signInFormgroup.controls;
   }
 }
