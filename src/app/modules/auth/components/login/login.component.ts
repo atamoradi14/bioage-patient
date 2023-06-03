1f3d87c23e978d739e4b56b133d99dbdabbe94d9
Naime Ahmed <naime.ahmed@metadesignsolutions.co.uk>
2023-06-03 12:18:26 +0600

    header started

--- src/app/modules/auth/components/login/login.component.ts
+++ src/app/modules/auth/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { AuthService } from '@modules/auth/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-
+  constructor(private authService: AuthService, private titleService: Title) {}
+  login() {
+    this.authService.login();
+  }
 }
